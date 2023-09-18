36d902df205ddfac827b618d961bccacf752a636
Haseeb Burki <hzburki.hzb@gmail.com>
2023-09-18 18:50:56 +0500

    chained all middlewares

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,21 @@ import { rateLimit } from "elysia-rate-limit";
 /** Import Routes */
 import { authRoutes } from "./routes";
 import databaseConfig from "./config/database.config";
-
+ 
 const app = new Elysia()
-
-/** Midlewares */
-app.use(cors())
-// only run rate limiter in production
-process.env.NODE_ENV === 'production' && app.use(rateLimit())
-app.use(swagger())
-
-/** Injecting Plugins */
-app
+  /** Middleware */
+  .use(cors())
+  .use(rateLimit())
+  .use(swagger())
+  /** Injecting Plugins */
   .use(databaseConfig)
+  /* Routes */
   .use(authRoutes)
+  /** Home Route */
   .get('/', async ({ db }) => {
     const users = await db().query.users.findMany()
     return users
-})
+  });
 
 /** Start the server */
 app.listen(process.env.PORT || 3000, ({ hostname, port }) => {
